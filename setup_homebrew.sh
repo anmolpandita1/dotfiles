ac8ceb744f9542685e0b120a9a6a63aa6c8b2b32
Anmol Pandita <39385082+anmolpandita1@users.noreply.github.com>
2022-04-30 13:40:49 +0530

    Add setup_homebrew.sh to dotbot
    Install homebrew, packages and apps:
    >bat
    >visual-studio-code
    >google-chrome

---
+++ setup_homebrew.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+#install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#--| install packages and applications
+brew install bat
+brew install google-chrome
+brew install visual-studio-code
