3ca62034b81e90a4e22332a8bd4e7415cefd532c
Anmol Pandita <39385082+anmolpandita1@users.noreply.github.com>
2022-04-30 14:00:06 +0530

    Use homebrew Brewfile for installs
    Install exa

---
+++ Brewfile
@@ -0,0 +1,12 @@
+#Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+#Brews for packages
+brew "bat"
+brew "exa"
+
+#casks for applications
+cask "visual-studio-code"
+cask "vlc"
--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -4,6 +4,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 #--| install packages and applications
-brew install bat
-brew install --no-quarantine google-chrome
-brew install --no-quarantine visual-studio-code
+brew bundle --verbose
